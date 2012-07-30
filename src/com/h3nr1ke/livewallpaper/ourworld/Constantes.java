151dec71308c6fe116d92c3b851994a491b6a638
Henrique <h3nr1ke@gmail.com>
2012-07-30 19:03:49 -0300

    removed the ads

--- src/com/h3nr1ke/livewallpaper/ourworld/Constantes.java
+++ src/com/h3nr1ke/livewallpaper/ourworld/Constantes.java
@@ -1,7 +1,7 @@
 package com.h3nr1ke.livewallpaper.ourworld;
 
 public class Constantes {
-	public static final Boolean ADD_AD = true;
+	public static final Boolean ADD_AD = false;
 	public static final Boolean AD_TEST = false;
 	public static final String[] AD_TEST_DEVICE = { "YOUR_DEVICE_ID_HERE" };
 
